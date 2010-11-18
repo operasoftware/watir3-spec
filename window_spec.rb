7c718465627011c77824a045b219de3fff1db456
Stuart Knightley <stuartk@opera.com>
2010-11-18 16:14:39 +0100

    Added properties, navigation, actions and window managment to Window

--- window_spec.rb
+++ window_spec.rb
@@ -87,9 +87,173 @@ describe "Window" do
 
   # xpath
 
-  # tagname
+  # properties
+  # ----------
+
+  describe "#title" do
+    it "is the title of the window" do
+      window.title.should == "Non-control elements"
+    end
+
+    it "changes when the title tag changes" do
+      window.get_elements_by_tag(:title).first.text = "changed"
+      window.title.should == "changed"
+    end
+  end
+
+  describe "#source" do
+    it "is the source of the page" do
+      window.source.should match /^    <title>Non-control elements<\/title>/
+    end
+
+    it "is the original source" do
+      window.get_elements_by_tag(:title).first.text = "changed"
+      window.source.should match /^    <title>Non-control elements<\/title>/
+    end
+  end
+
+  # page management
+  # ---------------
+
+  describe "#url" do
+    it "is the url of this window" do
+      window.url.should == fixture('non_control_elements.html')
+    end
+  end
+
+  describe "#url=" do
+    it "navigates to the given url" do
+      window.url = fixture('forms_with_input_elements.html')
+      window.get_elements_by_tag(:title).first.text.should == "Forms with input elements"
+    end
+  end
+
+  describe "#back" do
+    it "goes back one page in history" do
+      window.url = fixture('forms_with_input_elements.html')
+      window.back()
+      window.url.should == fixture('non_control_elements.html')
+    end
+
+    # should it raise an exception if it fails instead?
+    it "is true if it is possible to go back" do
+      window.url = fixture('forms_with_input_elements.html')
+      window.back.should be_true
+    end
+
+    it "is false if there is no page to go back to" do
+      window.back.should be_false
+    end
+  end
+
+  describe "#forward" do
+    it "goes forward one page in history" do
+      window.url = fixture('forms_with_input_elements.html')
+      window.back()
+      window.forward()
+      window.url.should == fixture('forms_with_input_elements.html')
+    end
+
+    # should it raise an exception if it fails instead?
+    it "is true if it is possible to go forward" do
+      window.url = fixture('forms_with_input_elements.html')
+      window.back()
+      window.forward().should be_true
+    end
+
+    it "is false if there is no page to go forward to" do
+      window.forward().should be_false
+    end
+
+  end
+
+  describe "#refresh" do
+    it "refreshes the current page" do
+      title = window.get_elements_by_tag(:title).first
+      title.text = "changed"
+      title.text.should == "changed"
+
+      window.refresh()
+      title.text.should == "Non-control elements"
+    end
+  end
+
+  # actions
+  # -------
 
   # click(x,y)
 
-  # all
+  describe "#eval_js" do
+    it "executes Javascript in the page" do
+      window.eval_js('document.title = "test"')
+      window.title.should == "test"
+    end
+
+    it "returns an element when the Javascript does" do
+      window.eval_js('document.createElement("div")').tag_name.should == "div"
+    end
+
+    it "returns a number when the Javascript does" do
+      window.eval_js('Math.abs(-5)').should == 5
+    end
+
+    it "returns a boolean when the Javascript does" do
+      window.eval_js('(function(){return true;})()').should be_true
+    end
+
+    it "returns an array when the Javascript does" do
+      result = window.eval_js('["this", "is", "a", "test"]')
+      result.length.should == 4
+      result[3].should == "test"
+    end
+
+    it "returns a string when the result is not one of these types" do
+      window.eval_js('({one:"two"}).toString()').should == "[object Object]"
+    end
+  end
+
+  # window management
+  # -----------------
+
+  describe "#maximize" do
+    it "maximizes the window" do
+      body = window.get_elements_by_tag(:body).first
+      # make sure we aren't already maximized
+      window.restore()
+      width = body.width
+      window.maximize()
+      body.width.should be > width
+    end
+  end
+
+  describe "#restore" do
+    it "restores (unmaximizes) the window" do
+      body = window.get_elements_by_tag(:body).first
+      # make sure we aren't already restored
+      window.maximize()
+      width = body.width
+      window.restore()
+      body.width.should be < width
+    end
+  end
+
+  describe "#close" do
+    it "destroys the window" do
+      window.close()
+      window.exists?.should be_false
+      window.get_elements_by_tag(:title).should raise_error
+    end
+  end
+
+  describe "#exists?" do
+    it "is true if the window exists" do
+      window.exists?.should be_true
+    end
+
+    it "is false if the window does not exist" do
+      window.close()
+      window.exists?.should be_false
+    end
+  end
+
 end

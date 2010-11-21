28385cd75b21cce75cc6170da9dd55b182f72e5b
Andreas Tolf Tolfsen <andreastt@opera.com>
2010-11-21 20:56:41 +0100

    Added specification for getting and setting preferences

--- browser_spec.rb
+++ browser_spec.rb
@@ -46,14 +46,17 @@ describe 'Browser' do
 
     it 'is a list of open windows' do
       browser.windows.all? do |window|
-        window.respond_to? :get_elements_by_id
+        window.respond_to? :find_elements_by_id
       end.should be_true
     end
+
+    # TODO: Window selectors
+
   end
 
   describe '#close' do
     it 'closes the browser' do
-      browser.close()
+      browser.close
       browser.exists?.should be_false
     end
   end
@@ -97,6 +100,73 @@ describe 'Browser' do
     it 'cannot be set to other values' do
       browser.speed = :hoobaflooba
       browser.speed.should_not == :hoobaflooba
+    end
+  end
+
+  describe '#preferences' do
+    before :all do
+      # Caches options that we will tamper with.
+      @preferences = browser.preferences
+    end
+
+    it 'is a list of all preferences' do
+      browser.preferences.length.should > 10
+    end
+
+    it 'contains options' do
+      browser.preferences('Developer Tools', 'Proxy Auto Connect').should_not be_empty
+    end
+
+    it 'gets a value of an option' do
+      browser.preferences('Cache', 'SVG Cache Size').should be_numeric
+      browser.preferences('Cache', 'SVG Cache Size').should > 0
+    end
+
+    it 'returns a top block of preferences' do
+      browser.preferences each do |section, option, value|
+        section.should_not be_empty
+        option.should_not be_empty
+      end
+    end
+
+    it 'returns a sub block of preferences' do
+      browser.preferences('Cache') each do |option, value|
+        option.should_not be_empty
+      end
+    end
+
+    it 'sets an option' do
+      browser.preferences('Cache', 'Application Cache Quota') = 0
+      browser.preferences('Cache', 'Application Cache Quota').should == 0
+    end
+
+    it 'sets options by a given block' do
+      preferences = {
+        'Cache' => {
+          'Always Check Never-Expiring GET queries' => false,
+          'Application Cache Quota' => -5000,
+          'SVG Cache Size' => 2000
+        }
+      }
+
+      browser.preferences = preferences
+
+      browser.preferences('Cache', 'Always Check Never-Expiring GET queries').should_not be_true
+      browser.preferences('Cache', 'Application Cache Quota').should == -5000
+      browser.preferences('Cache', 'SVG Cache Size').should == 2000
+    end
+
+    it 'fetches default value of an option' do
+      browser.preferences('Cache', 'SVG Cache Size').reset.should > 2000
+    end
+
+    it 'resets an option to default' do
+      browser.preferences('Cache', 'SVG Cache Size').reset!
+    end
+
+    after :all do
+      # Resets options that we've tampered with.
+      browser.preferences = @preferences
     end
   end
 
